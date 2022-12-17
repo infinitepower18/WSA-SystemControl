0473ff151945814de6bb1b2295c0d5f1bb1f069a
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2022-12-17 17:38:32 +0000

    Add project files.

---
+++ WSA System Control/AppContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSA_System_Control
+{
+    internal class AppContext : ApplicationContext
+    {
+        NotifyIcon notifyIcon;
+        ContextMenuStrip contextMenu;
+        public AppContext()
+        {
+            ToolStripMenuItem startMenuItem = new ToolStripMenuItem("Start WSA", Image.FromFile("icon.ico"), new EventHandler(startWSA));
+            ToolStripMenuItem stopMenuItem = new ToolStripMenuItem("Stop WSA", Image.FromFile("icongrey.png"), new EventHandler(stopWSA));
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Exit", Image.FromFile("exit.png"), new EventHandler(Exit));
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new Icon("icon.ico");
+            contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add(startMenuItem);
+            contextMenu.Items.Add(stopMenuItem);
+            contextMenu.Items.Add(exitMenuItem);
+            notifyIcon.ContextMenuStrip= contextMenu;
+            notifyIcon.Visible = true;
+        }
+        void Exit(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+        void startWSA(object sender, EventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.CreateNoWindow= true;
+            proc.StartInfo.FileName = "CMD.exe";
+            proc.StartInfo.Arguments = "/C WSAClient /launch wsa://system";
+            proc.Start();
+        }
+        void stopWSA(object sender, EventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.FileName = "CMD.exe";
+            proc.StartInfo.Arguments = "/C WSAClient /shutdown";
+            proc.Start();
+        }
+    }
+}
