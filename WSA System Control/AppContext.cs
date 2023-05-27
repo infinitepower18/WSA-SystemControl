c1f73547756bc1a447692dbf1eecf0cff72ca92d
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2023-05-27 16:58:57 +0100

    remove unused namespaces

--- WSA System Control/AppContext.cs
+++ WSA System Control/AppContext.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace WSA_System_Control
 {
