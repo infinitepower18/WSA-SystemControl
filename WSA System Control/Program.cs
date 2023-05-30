07a5412ab8bd6dad045f2ef8e34b771dfbaaef96
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2023-05-30 20:35:53 +0100

    prevent multiple instances

--- WSA System Control/Program.cs
+++ WSA System Control/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace WSA_System_Control
 {
     internal static class Program
@@ -11,7 +13,15 @@ namespace WSA_System_Control
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AppContext());
+            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                MessageBox.Show("WSA System Control is already running!");
+                Application.Exit();
+            }
+            else
+            {
+                Application.Run(new AppContext());
+            }
         }
     }
 }
